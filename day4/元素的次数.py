159362a7757db34917e139ee0bc9ab9e9f47b791
Xioha <94661959+Xioha@users.noreply.github.com>
2021-11-27 17:25:17 +0800

    第四天

---
+++ day4/元素的次数.py
@@ -0,0 +1,19 @@
+'''
+请编程统计列表中的每个数字出现的次数(百度初级测试开发笔试题)
+List = [1,4,7,5,8,2,1,3,4,5,9,7,6,1,10]
+
+'''
+
+list = [1, 4, 7, 5, 8, 2, 1, 3, 4, 5, 9, 7, 6, 1, 10]
+
+print("列表中“1”出现的次数：", list.count(1))    #list.count( )) 表示某在列表中出现的次数
+print("列表中“2”出现的次数：", list.count(2))
+print("列表中“3”出现的次数：", list.count(3))
+print("列表中“4”出现的次数：", list.count(4))
+print("列表中“5”出现的次数：", list.count(5))
+print("列表中“6”出现的次数：", list.count(6))
+print("列表中“7”出现的次数：", list.count(7))
+print("列表中“8”出现的次数：", list.count(8))
+print("列表中“9”出现的次数：", list.count(9))
+print("列表中“10”出现的次数：", list.count(10))
+
